13790eeec0c30e5633d09ff4e7c8806d5e394c1d
hvsantos <hv_santos@outlook.com>
2023-03-13 09:28:35 -0300

    feat add new get routes

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.get('/q', (req, res) => res.status(200).json({ message: req.query.name }));
+app.get('/search/:id/:name', (req, res) => res.status(200).json({
+    message: `id ${req.params.id} e nome ${req.params.name}`,
+}));
+
 module.exports = app;
