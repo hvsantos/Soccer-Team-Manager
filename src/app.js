32f7987a462925b0bed7200a7a687ad55891b344
hvsantos <hv_santos@outlook.com>
2023-03-13 09:43:39 -0300

    feat add new route to test requests with body

--- src/app.js
+++ src/app.js
@@ -13,4 +13,14 @@ app.get('/search/:id/:name', (req, res) => res.status(200).json({
     message: `id ${req.params.id} e nome ${req.params.name}`,
 }));
 
+// Body object: {
+//   "name": "Ronaldo",
+//   "profession": "Football player"
+// }
+app.get('/test', (req, res) => {
+    console.log(req.body.name);
+    console.log(req.body.profession);
+    return res.status(200).json({ message: req.body });
+});
+
 module.exports = app;
