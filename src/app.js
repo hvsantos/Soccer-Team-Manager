8dd9ddcb1c9e59d3bbfa2f5d9ed48089208c0c3b
hvsantos <hv_santos@outlook.com>
2023-03-13 09:43:02 -0300

    feat add express.json middleware to read req body

--- src/app.js
+++ src/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
 app.get('/q', (req, res) => res.status(200).json({ message: req.query.name }));
+
+// URL search: localhost:3001/search/5/Ronaldo
 app.get('/search/:id/:name', (req, res) => res.status(200).json({
     message: `id ${req.params.id} e nome ${req.params.name}`,
 }));
