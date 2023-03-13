c96f80395e7593a3d18d06742cc7d41e6cbe3e4e
hvsantos <hv_santos@outlook.com>
2023-03-13 09:52:20 -0300

    feat add team object & route + response for /teams

--- src/app.js
+++ src/app.js
@@ -2,25 +2,40 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
+const teams = [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPF',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+];
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
-app.get('/q', (req, res) => res.status(200).json({ message: req.query.name }));
+module.exports = app;
+
+// app.use(express.json());
+
+// app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+// app.get('/q', (req, res) => res.status(200).json({ message: req.query.name }));
 
 // URL search: localhost:3001/search/5/Ronaldo
-app.get('/search/:id/:name', (req, res) => res.status(200).json({
-    message: `id ${req.params.id} e nome ${req.params.name}`,
-}));
+// app.get('/search/:id/:name', (req, res) => res.status(200).json({
+//     message: `id ${req.params.id} e nome ${req.params.name}`,
+// }));
 
 // Body object: {
 //   "name": "Ronaldo",
 //   "profession": "Football player"
 // }
-app.get('/test', (req, res) => {
-    console.log(req.body.name);
-    console.log(req.body.profession);
-    return res.status(200).json({ message: req.body });
-});
-
-module.exports = app;
+// app.get('/test', (req, res) => {
+//     console.log(req.body.name);
+//     console.log(req.body.profession);
+//     return res.status(200).json({ message: req.body });
+// });
